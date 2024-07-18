adca0249b363b0aa09f2e1678f7606290f4e8c4d
HugoM25 <hddm25@gmail.com>
2024-07-18 13:46:47 +0200

    feat: now page is responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,21 +7,43 @@ import Jail from './components/Jail';
 function App() {
   const [fenString, setFenString] = useState<string>("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
+  const handlePreviousClick = () => {
+      // Logic for handling previous button click
+      console.log('Previous button clicked');
+  };
+
+  const handleNextClick = () => {
+      // Logic for handling next button click
+      console.log('Next button clicked');
+  };
   return (
     <>
       <div>
-        <div className="board-zone">
+        <div className="board-page">
           <div className='info-zone white'>
+            <div className='jail-zone'>
+              <Jail pieces={['B', 'B', 'Q', 'P', 'N', 'P', 'P']} isWhite={false}  /> 
+            </div>
+            <div className="clock-zone">
               <Clock isWhite={true} />
-              <Jail pieces={['B', 'B', 'Q', 'P', 'N', 'P', 'P']} isWhite={false}  />
+            </div>
           </div>
           <div className='board-display-zone'>
             <Board fenString={fenString} />
           </div>
           <div className="info-zone black">
+            <div className="clock-zone">
               <Clock isWhite={false} />
+            </div>
+            <div className='jail-zone'>
               <Jail pieces={['b', 'b', 'p', 'p', 'r', 'n', 'p', 'p']} isWhite={true} />
+            </div>
           </div>
+        </div>
+        <div className="page-selection-zone">
+          <button onClick={handlePreviousClick}></button>
+          <button onClick={handlePreviousClick}></button>
+          <button onClick={handleNextClick}></button>
         </div>
       </div>
     </>
--- frontend/src/components/Jail/index.tsx
+++ frontend/src/components/Jail/index.tsx
@@ -7,29 +7,41 @@ interface JailProps {
 }
 
 export default function Jail(props: JailProps) {
+
     // Create a map to count occurrences of each piece
     const pieceCountMap: { [key: string]: number } = {};
     props.pieces.forEach(piece => {
         pieceCountMap[piece] = (pieceCountMap[piece] || 0) + 1;
     });
 
-    // Create an array of unique pieces
-    const uniquePieces = Object.keys(pieceCountMap);
+    // Order the pieces in this order : King, Queen, Rook, Bishop, Knight, Pawn
+    const pieceOrder = ['K', 'Q', 'R', 'B', 'N', 'P'];
+    // Sort the pieces by the order
+    const sortedPieces = Object.keys(pieceCountMap).sort((a, b) => pieceOrder.indexOf(a.toUpperCase()) - pieceOrder.indexOf(b.toUpperCase()));
 
-    return (
-        <div className={`jail ${props.isWhite ? 'white' : 'black'}`}>
-            {
-                uniquePieces.map((piece, index) => (
-                    Array.from({ length: pieceCountMap[piece] }).map((_, i) => (
+    // Create an array of 5 divs
+    const divs = Array.from({ length: 5 }).map((_, index) => {
+        const piece = sortedPieces[index];
+        return (
+            <div key={index} className="piece-container">
+                {
+                    piece && Array.from({ length: pieceCountMap[piece] }).map((_, i) => (
                         <img
                             key={`${piece}-${i}`}
                             src={pieceMap[piece]} // Use pieceMap to get the correct SVG path
                             alt={piece}
                             className={`jail-piece ${i > 0 ? 'same' : ''}`} // Add "same" class to subsequent pieces
+                            style={{ left: `${i * 10}px` }} // Apply dynamic left position
                         />
                     ))
-                ))
-            }
+                }
+            </div>
+        );
+    });
+
+    return (
+        <div className={`jail ${props.isWhite ? 'white' : 'black'}`}>
+            {divs}
         </div>
     );
 }
